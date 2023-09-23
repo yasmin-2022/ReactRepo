4915fe9a30a480fe6a2aab666e320ed5ece63317
yasmin-2022 <parveenyasmin390@gmail.com>
2023-09-24 01:05:37 +0530

    Create a search input and button to search dish or food

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,10 +1,14 @@
 import Meal from '../utils/mokeDetails'
 import RestaurentCard from './Restaurent'
-import { useState,useEffect } from 'react'
+import { useState, useEffect } from 'react'
+import Shimmer from'./Shimmer'
 
 
 const Body = () => {
-  const [List,setList] = useState(Meal)
+  const [List,setList] = useState([])
+  const [FilterRes,setFilterRes] = useState([])
+  const [searchText, setSearchText] = useState("")
+  console.log("Body Rerender")
   useEffect(()=>{
     fetchData()
   }, [])
@@ -12,10 +16,24 @@ const Body = () => {
     const data = await fetch("https://www.swiggy.com/dapi/restaurants/list/v5?lat=22.6012849&lng=88.3312686&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING")
     const json = await data.json()
     console.log(json)
+    setList(Meal)
+    setFilterRes(Meal)
   }
-  return (
+  return List.length === 0 ?<Shimmer /> :(
     <div className='body'>
       <div className='filter'>
+        <div className='search'>
+          <input className='search-food' value={searchText} onChange={(e)=>setSearchText(e.target.value)}/>
+          <button className='searchBtn' onClick={() =>{
+
+            console.log(searchText);
+            const searchFiltered = List.filter((ele) => ele.strCategory.toLowerCase().includes(searchText.toLowerCase()));
+            setFilterRes(searchFiltered)
+          }
+            
+          
+          }>Search</button>
+       </div>
         <button className='filter-btn ' onClick={() => {
           let filtered = List.filter((res) => res.strCategoryRating > 4);
           console.log(filtered)
@@ -24,7 +42,7 @@ const Body = () => {
         }}>TopRestaurant</button>
       </div>
       <div className='res-container'>
-        {List.map((res) => <RestaurentCard key={res.idCategory} resData={ res} />)}
+        {FilterRes.map((res) => <RestaurentCard key={res.idCategory} resData={ res} />)}
       </div>
     </div>
   )
---
+++ src/components/Shimmer.js
@@ -0,0 +1,17 @@
+const Shimmer = () => {
+  return (
+    <div className="Shimmer-page">
+<div className="shimmer"></div>
+<div className="shimmer"></div>
+<div className="shimmer"></div>
+<div className="shimmer"></div>
+<div className="shimmer"></div>
+<div className="shimmer"></div>
+<div className="shimmer"></div>
+<div className="shimmer"></div>
+<div className="shimmer"></div>
+<div className="shimmer"></div>
+    </div>
+  )
+}
+export default Shimmer;
